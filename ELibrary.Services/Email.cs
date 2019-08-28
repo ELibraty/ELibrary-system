c33a227a349db5d9402fdedb6003301147896015
Dimitar <dim_kolev2002@abv.bg>
2019-08-28 08:28:28 +0300

    Do password with MD5

---
+++ ELibrary.Services/Email.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Mail;
+
+namespace ELibrary.Services
+{
+    class Email
+    {
+        public void Send(string mailTo, string message, string subject)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("dimitarkole@gmail.com");
+                mail.To.Add(mailTo);
+                mail.Subject = subject;
+                mail.Body = message;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("username", "password");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                string a = ex.ToString();
+
+            }
+
+        }
+    }
+}
--- ELibrary.Services/IndexService.cs
+++ ELibrary.Services/IndexService.cs
@@ -3,6 +3,9 @@ using ELibrary.Data.Models;
 using ELibrary.Services.Contracts;
 using System;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace ELibrary.Services
 {
@@ -17,6 +20,10 @@ namespace ELibrary.Services
 
         public int SingUp(string userName, string password, string email, string type, string avatar)
         {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                password = GetMd5Hash(md5Hash, password);
+            }
             var user = new User()
             {
                 UserName = userName,
@@ -32,11 +39,33 @@ namespace ELibrary.Services
             return user.Id;
         }
 
-       /* public bool SingIn(string Email, string Password)
+         public bool SingIn(string Email, string Password)
+         {
+             var user = this.context.Users.FirstOrDefault(x => x.Email == Email && x.Password == Password);
+             if (user != null) return true;
+             else return false;  
+         }
+
+        //do pass to MD5
+        static string GetMd5Hash(MD5 md5Hash, string input)
         {
-            var user = this.context.Users.FirstOrDefault(x => x.Email == Email && x.Password == Password);
-            if (user != null) return true;
-            else return false;
-        }*/
-    }
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+    }  
 }
